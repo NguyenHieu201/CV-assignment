dc53d91c1d77989e8493cffdee77e579c99a74aa
NguyenHieu201 <hieu.nguyenminh2012001@gmail.com>
2023-04-10 17:39:10 +0700

    complete

--- ex2.py
+++ ex2.py
@@ -3,14 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = cv2.imread("./input/input.jpg")
-hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 h, s, v = cv2.split(hsv_img)
 
-low, high = 80, 120
-hand_area = (h >= low) & (h <= high)
+low, high = 0, 50
+hand_area = ((h >= low) & (h <= high)) | (h >= 100)
 hand_area = ~hand_area
 
 hsv_img[hand_area, 2] = 0
 save_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
 
 cv2.imwrite("./output/output_hand.jpg", save_img)
+
+
+# value, bins = np.histogram(h, 256, [0, 256])
+# plt.plot(range(256), value)
+# plt.show()
